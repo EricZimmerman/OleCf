ee99a5260ac3822645e0a8b14e87a268772c5601
Eric Zimmerman <saericzimmerman@gnail.com>
2016-02-20 09:38:49 -0700

    Add project files.

---
+++ OleCf/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OleCf
+{
+    public class Class1
+    {
+    }
+}
